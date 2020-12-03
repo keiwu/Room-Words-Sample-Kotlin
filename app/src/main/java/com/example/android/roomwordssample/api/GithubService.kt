a445d95878a6ac1221b78e0e096b3ce4a11e59f4
kei <keiwus@gmail.com>
2020-12-02 16:40:54 -0800

    Fixed the error to make the code run and compile

--- app/src/main/java/com/example/android/roomwordssample/api/GithubService.kt
+++ app/src/main/java/com/example/android/roomwordssample/api/GithubService.kt
@@ -17,8 +17,8 @@
 package com.example.android.roomwordssample.api
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.logging.HttpLoggingInterceptor.Level
+//import okhttp3.logging.HttpLoggingInterceptor
+//import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -45,11 +45,11 @@ interface GithubService {
         private const val BASE_URL = "https://api.github.com/"
 
         fun create(): GithubService {
-            val logger = HttpLoggingInterceptor()
-            logger.level = Level.BASIC
+//            val logger = HttpLoggingInterceptor()
+//            logger.level = Level.BASIC
 
             val client = OkHttpClient.Builder()
-                    .addInterceptor(logger)
+//                    .addInterceptor(logger)
                     .build()
             return Retrofit.Builder()
                     .baseUrl(BASE_URL)
